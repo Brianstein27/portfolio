99253fef5ca93a6e68619887860faeb1e309324e
Brianstein27 <brianstein27@proton.me>
2024-07-31 15:19:14 +0200

    finish desktop styling

--- src/index.js
+++ src/index.js
@@ -6,10 +6,38 @@ pos.addEventListener("mousemove", (e) => {
 });
 
 // smooth scroll
-document.querySelectorAll(".nav-section li>a").forEach((link) => {
-  const section = document.querySelector(link.hash);
+const navLinks = document.querySelectorAll(".nav-section li>a");
 
-  link.addEventListener("click", () => {
-    section.scrollIntoView({ behavior: "smooth" });
+// spy nav links
+const sections = document.querySelectorAll("section");
+const main = document.querySelector("main");
+
+console.log(sections);
+
+main.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - main.offsetTop;
+    const sectionHeight = section.clientHeight;
+    console.log(
+      `Section: ${section.id} Top: ${sectionTop} Height: ${sectionHeight}`,
+    );
+    console.log('main scroll top', main.scrollTop);
+
+    if (
+      main.scrollTop >= sectionTop - 1 &&
+      main.scrollTop < sectionTop + sectionHeight
+    ) {
+      currentSection = section.getAttribute("id");
+      // console.log(section, sectionTop, sectionHeight);
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").substring(1) === currentSection) {
+      link.classList.add("active");
+    }
   });
 });
